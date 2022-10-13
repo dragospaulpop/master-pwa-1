64be210d7c10df590ae998ba09734b4dee62309e
Dragos-Paul Pop <dragos_paul_pop@yahoo.com>
2022-10-13 20:53:39 +0300

    Vuetify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,46 @@
 <template>
-  <div>
-   <h1>TO DO List:</h1>
-   <input type="text" v-model="todo" @keydown.enter="addTodo">
-   <button @click="addTodo">add todo</button>
-   <ul>
-    <li
-      v-for="(item, index) in todoList"
-      :key="item.value"
-      :class="{ 'todo-done': item.done }">
-      <button @click="removeTodo(index)">🗑</button>
-      {{ item.value }}
-      <button v-if="item.done" @click="toggleDone(index)">
-        ✔
-      </button>
-      <button v-else @click="toggleDone(index)">
-        x
-      </button>
-    </li>
-   </ul>
-  </div>
+  <v-app>
+    <v-main>
+      <v-container fluid fill-height>
+        <v-row>
+          <v-col align-center class="text-center">
+            <v-card>
+              <v-card-title>TO DO List:</v-card-title>
+              <v-card-text>
+                <v-text-field
+                  outlined
+                  v-model="todo"
+                  @keydown.enter="addTodo"
+                  append-outer-icon="mdi-plus"
+                  @click:append-outer="addTodo"
+                  hint="Add your todo"
+                  label="To do:"></v-text-field>
+                <v-list>
+                  <v-list-item v-for="(item, index) in todoList" :key="item.value">
+                    <v-list-item-action>
+                      <v-btn icon color="error" @click="removeTodo(index)">
+                        <v-icon>mdi-delete</v-icon>
+                      </v-btn>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                      <v-list-item-title :class="{ 'text-decoration-line-through': item.done }">{{ item.value }}</v-list-item-title>
+                    </v-list-item-content>
+                    <v-list-item-action>
+                      <v-btn icon :color="item.done ? 'success' : 'error'"  @click="toggleDone(index)">
+                        <v-icon>
+                          {{ item.done ? 'mdi-check' : 'mdi-close' }}
+                        </v-icon>
+                      </v-btn>
+                    </v-list-item-action>
+                  </v-list-item>
+                </v-list>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -52,9 +74,3 @@ export default {
   }
 }
 </script>
-
-<style>
-  .todo-done {
-    text-decoration: line-through;
-  }
-</style>
